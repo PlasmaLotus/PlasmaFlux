b853e138ed2559fff47c8bd7ae099ebc0e7917d6
Lee-Stenio nazer <ls_nazer@hotmail.com>
2017-02-02 18:33:04 -0500

    Add files via upload

--- src/Game/Player.h
+++ src/Game/Player.h
@@ -9,7 +9,18 @@ public:
 	~Player();
 	void ok();
 	enum State{Idle, Walking, Running, Jumping, Landing};
+	struct Vec {
+		float x, y;
+	};
+	struct Position {
+		int x, y;
+	};
 private:
-
+	Vec velocity;
+	const float jmp = -6;
+	const float topXSpeed = 6;
+	const float topYSpeed = 16;
+	const float gravity = 0.21875;
+	Position position;
 };
 #endif
